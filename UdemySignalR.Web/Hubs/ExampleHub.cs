6508d254f66db709b74d4ddac55d2c7ff1da1e93
FatihKoseoglu <koseoglufatih@gmail.com>
2024-07-14 14:20:00 +0300

    Add project files.

---
+++ UdemySignalR.Web/Hubs/ExampleHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace UdemySignalR.Web.Hubs
+{
+    public class ExampleHub:Hub
+    {
+        public async Task BroadcastMessageToAllClient(string message) 
+            // bu method client tarafından tetiklenecek. index javascprit tarafından method tetiklenince başka bir methodu tetikleyecek.
+            //client methodları çağıranlar.
+        {
+            await Clients.
+
+        }
+
+    }
+}
