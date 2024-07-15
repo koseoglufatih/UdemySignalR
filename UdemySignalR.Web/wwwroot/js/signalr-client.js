f9862ab75fab95eed07a6f987ea6e90fb427ad52
FatihKoseoglu <koseoglufatih@gmail.com>
2024-07-15 13:02:19 +0300

    Group-2 Hub

--- UdemySignalR.Web/wwwroot/js/signalr-client.js
+++ UdemySignalR.Web/wwwroot/js/signalr-client.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+
+    const connection = new signalR.HubConnectionBuilder().withUrl("/exampleTypeSafeHub").configureLogging(signalR.LogLevel.Information).build();
+
     const broadcastMessageToAllClientHubMethodCall = "BroadcastMessageToAllClient";
     const receiveMessageForAllClientMethodCall = "ReceiveMessageForAllClient";
 
@@ -11,109 +14,192 @@ $(document).ready(function () {
 
     const broadcastMessageToIndividualClient = "BroadcastMessageToIndividualClient";
     const receiveMessageForIndividualClient = "ReceiveMessageForIndividualClient";
-   
+
 
     const receiveConnectedClientCountAllClient = "ReceiveConnectedClientCountAllClient";
 
+    const groupA = "GroupA";
+    const groupB = "GroupB";
+    let currentGroupList = [];                          //rastgele basarsa bu grouba dahil olacak
 
-    ; const connection = new signalR.HubConnectionBuilder().withUrl("/exampleTypeSafeHub").configureLogging(signalR.LogLevel.Information).build();
-
-    function start() {
-        connection.start().then(() => {
-            console.log("Hub ile baglanti kuruldu.");
-            $("#connectionId").html(`Connection Id : ${connection.connectionId}`);
-        });
+    function refreshGroupList() {
+        $("#groupList").empty();
+        currentGroupList.forEach(x => {
+            $("#groupList").append(`<p>${x}</p>`)
+        })
     }
 
-    try {
-        start();
-    }
-    catch
-    {
-        setTimeout(() => start(), 5000)
-    }
+    $("#btn-groupA-add").click(function () {
 
+        if (currentGroupList.includes(groupA)) return;
 
-    //subscribe
-    connection.on(receiveMessageForAllClientMethodCall, (message) => {
+        connection.invoke("AddGroup", groupA).then(() => {
 
-        console.log("Gelen Mesaj", message);
+            currentGroupList.push(groupA);
+            refreshGroupList();
+        })
     })
 
-    connection.on(receiveMessageForCallerClient, (message) => {
+    $("#btn-groupB-add").click(function () {
 
-        console.log("(Caller) Gelen Mesaj", message);
+        if (currentGroupList.includes(groupB)) return;
+
+        connection.invoke("AddGroup", groupB).then(() => {
+
+
+            currentGroupList.push(groupB);
+            refreshGroupList();
+        })
     })
 
-    connection.on(receiveMessageForOthersClient, (message) => {
+    $("#btn-groupA-remove").click(function () {
 
-        console.log("(Others) Gelen Mesaj", message);
+
+        $("#btn-groupA-remove").click(function () {
+            if (!currentGroupList.includes(groupA)) return;
+
+            connection.invoke("RemoveGroup", groupA).then(() => {
+                currentGroupList = currentGroupList.filter(x => x !== groupA); // currentGroupList'i güncelle
+                refreshGroupList();
+            })
+        })
+
+        $("#btn-groupB-remove").click(function () {
+            if (!currentGroupList.includes(groupB)) return;
+
+            connection.invoke("RemoveGroup", groupB).then(() => {
+                currentGroupList = currentGroupList.filter(x => x !== groupB); // currentGroupList'i güncelle
+                refreshGroupList();
+            })
+        })
+
+        $("#btn-groupA-send-message").click(function () {
+            const message = "Group A Mesaj";
+            connection.invoke("BroadcastMessageToGroupClients", groupA, message).catch(err => console.error("hata", err))
+            console.log("Mesaj gönderildi");
+
+        })
+
+        $("#btn-groupB-send-message").click(function () {
+            const message = "Group B Mesaj";
+            connection.invoke("BroadcastMessageToGroupClients", groupB, message).catch(err => console.error("hata", err))
+            console.log("Mesaj gönderildi");
+
+        })
+
+        connection.on("ReceiveMessageForGroupClients", (message) => {
+
+            console.log("Gelen Mesaj", message);
+        })
+
+
+
+
+
+
+
+
+
+
+
+        function start() {
+            connection.start().then(() => {
+                console.log("Hub ile baglanti kuruldu.");
+                $("#connectionId").html(`Connection Id : ${connection.connectionId}`);
+            });
+        }
+
+        try {
+            start();
+        }
+        catch
+        {
+            setTimeout(() => start(), 5000)
+        }
+
+
+        //subscribe
+        connection.on(receiveMessageForAllClientMethodCall, (message) => {
+
+            console.log("Gelen Mesaj", message);
+        })
+
+        connection.on(receiveMessageForCallerClient, (message) => {
+
+            console.log("(Caller) Gelen Mesaj", message);
+        })
+
+        connection.on(receiveMessageForOthersClient, (message) => {
+
+            console.log("(Others) Gelen Mesaj", message);
+        })
+
+        connection.on(receiveMessageForIndividualClient, (message) => {
+
+            console.log("(Individual) Gelen Mesaj", message);
+        })
+
+
+        const span_client_count = $("#span - connected - client - count");
+        connection.on(receiveConnectedClientCountAllClient, (count) => {
+            span_client_count.text(count);
+            console.log("connected client count", count);
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        $("#btn-send-message-all-client").click(function () {
+
+            const message = "hello world";
+            connection.invoke(broadcastMessageToAllClientHubMethodCall, message).catch(err => console.error("hata", err))
+            console.log("Mesaj gönderildi");
+
+        })
+
+        $("#btn-send-message-caller-client").click(function () {
+
+            const message = "hello world";
+            connection.invoke(broadcastMessageToCallerClient, message).catch(err => console.error("hata", err))
+            console.log("Mesaj gönderildi");
+
+        })
+
+        $("#btn-send-message-others-client").click(function () {
+
+            const message = "hello world";
+            connection.invoke(broadcastMessageToOtherClient, message).catch(err => console.error("hata", err))
+            console.log("Mesaj gönderildi");
+
+        })
+
+        $("#btn-send-message-individual-client").click(function () {
+
+            const message = "hello world";
+            const connectionId = $("#text-connectionId").val();
+            connection.invoke(broadcastMessageToIndividualClient, connectionId, message).catch(err => console.error("hata", err))
+            console.log("Mesaj gönderildi");
+
+
+
+        })
+
+
+
     })
-
-    connection.on(receiveMessageForIndividualClient, (message) => {
-
-        console.log("(Individual) Gelen Mesaj", message);
-    })
-
-
-
-    const span_client_count = $("#span - connected - client - count");
-    connection.on(receiveConnectedClientCountAllClient, (count) => {
-        span_client_count.text(count);
-        console.log("connected client count", count);
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    $("#btn-send-message-all-client").click(function () {
-
-        const message = "hello world";
-        connection.invoke(broadcastMessageToAllClientHubMethodCall, message).catch(err => console.error("hata", err))
-        console.log("Mesaj gönderildi");
-
-    })
-
-    $("#btn-send-message-caller-client").click(function () {
-
-        const message = "hello world";
-        connection.invoke(broadcastMessageToCallerClient, message).catch(err => console.error("hata", err))
-        console.log("Mesaj gönderildi");
-
-    })
-    $("#btn-send-message-others-client").click(function () {
-
-        const message = "hello world";
-        connection.invoke(broadcastMessageToOtherClient, message).catch(err => console.error("hata", err))
-        console.log("Mesaj gönderildi");
-
-    })
-
-    $("#btn-send-message-individual-client").click(function ()
-    {
-
-        const message = "hello world";
-        const connectionId = $("#text-connectionId").val();
-        connection.invoke(broadcastMessageToIndividualClient, connectionId, message).catch(err => console.error("hata", err))
-        console.log("Mesaj gönderildi");
-
-
-
-    })
-
-
 
 })
+
